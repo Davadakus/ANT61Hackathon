3a0a53dd6d60ad0389367914c4c76ab93789ba1c
epur4192 <epur4192@uni.sydney.edu.au>
2024-10-06 03:12:03 +1100

    Basic Colour Pattern

--- src/App.tsx
+++ src/App.tsx
@@ -45,28 +45,55 @@ function App() {
   }, [])
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <h1 className="text-xl text-center font-bold"> 
+    <div className="min-h-screen flex flex-col bg-neutral-700">
+      <div className="text-xl text-center font-bold mt-3"> 
         ANT61 Beacon Data Simulation
-      </h1>
+      </div>
       <div className="grid grid-cols-2 gap-4 p-4 flex-1">
+        
         {/* First column */}
-        <div className="flex flex-col space-y-4">
-          <div className="bg-blue-500 flex-1 overflow-hidden relative">
+        <div className="flex flex-col p-4 bg-stone-900 rounded space-y-4">
+          <div>
+            <h1 className="text-2xl text-center font-bold">Simulated Beacon</h1>
+          </div>
+          <div className="bg-blue-500 flex-1 overflow-hidden relative rounded">
             Simulation
-            <MyThree rotation={rotation} gryoAccel={gryoAccel}/>
+            {/* <MyThree rotation={rotation} gryoAccel={gryoAccel}/> */}
           </div>
         </div>
-
+        
         {/* Second column */}
         <div className="flex flex-col gap-4">
-          <div className="flex gap-4 basis-2/3">
-            <div className="bg-green-500 h-full grow">
-              Altitude Data
+          <h1 className="flex flex-row gap-4 h-2/3">
+            <h2 className="flex flex-col bg-stone-900 p-4 w-1/4 rounded">
+              <h3 className="text-xl flex grow flex-col text-center">Altitude</h3>
+              <h3 className="bg-neutral-700 h-full grow rounded">Altitude Content </h3>
+            </h2>
+            <h2 className="text-xl flex flex-col bg-stone-900 p-4 w-full rounded">
+              <h3 className="flex flex-col"> Map</h3>
+              <Map />
+            </h2>
+          </h1>
+
+
+          {/* New section for two equal-width boxes
+          <div className="flex gap-4">
+            <div className="bg-green-500 flex-1 rounded">
+              <h1 className="text-xl text-center font-bold px-6">Altitude Data</h1>
             </div>
-            <Map></Map>
+            <div className="bg-green-500 flex-1 rounded basis-2/3 pt-4">
+              <h1 className="text-2xl text-center font-bold">Map</h1>
+            </div>
           </div>
-          <div className="bg-green-500 h-full flex flex-col basis-1/3">
+          <div className="flex gap-4 basis-2/3">
+            <div className="bg-gray-500 h-full grow rounded">
+              Altitude content
+            </div>
+            <Map />
+          </div> */}
+          
+          <div className="bg-gray-500 border-2 border-solid border-black h-full flex flex-col basis-1/3 rounded px-2">
+            <h1 className="text-2xl text-center font-bold">Live Data Feed:</h1>
             <h1>Message ID: {messageId}</h1>
             <h1>Location: {currentLocation && currentLocation[0]}, {currentLocation && currentLocation[1]}, {currentLocation && currentLocation[2]}</h1>
             <h1>Rotation: {rotation && rotation[0]}, {rotation && rotation[1]}, {rotation && rotation[2]}</h1>
