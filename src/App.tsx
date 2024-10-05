405ec999604800d2e85fe18ed5826a1cd286d2d8
epur4192 <epur4192@uni.sydney.edu.au>
2024-10-05 12:50:29 +1000

    Simulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import MyThree from './Three.jsx';
+
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      {/* <MyThree /> */}
       <h1 className="text-xl text-center font-bold"> 
         ANT61 Beacon Data Simulation
       </h1>
       <div className="grid grid-cols-2 gap-4 p-4 flex-1">
         {/* First column */}
         <div className="flex flex-col space-y-4">
-          <div className="bg-blue-500 flex-1">Simulation</div>
+          <div className="bg-blue-500 flex-1">
+            Simulation
+          </div>
         </div>
 
         {/* Second column */}
