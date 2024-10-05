90e81e947231501dba433ab8d274a0e4272e5d65
brian <brian.w.tjahjadi@gmail.com>
2024-10-05 12:32:33 +1000

    read and parse

---
+++ src/utils/parse.ts
@@ -0,0 +1,33 @@
+import fs from 'node:fs/promises';
+
+async function readTextFile(filePath: string): Promise<string> {
+    try {
+        const data = await fs.readFile(filePath, 'utf8'); // Use promise-based fs.readFile
+        return data;
+    } catch (err) {
+        console.error('Error reading file:', err);
+        return "";
+    }
+}
+
+function parseMessage(message: string): [string | null, number[] | null, number[] | null, number[] | null]  {
+    message = message.trim();
+    let messageId: string | null = message.split("V")[0] || null;
+    let location: number[] | null = message.split("L")[1]?.split("I")[0]?.slice(1,-1).split(",").map(Number) || null;
+    let rotation: number[] | null = message.split("R")[1]?.split("A")[0]?.slice(1,-1).split(",").map(Number) || null;
+    let gryoAccel: number[] | null = message.split("G")[1]?.split("I")[0]?.slice(1,-1).split(",").map(Number) || null;    
+    return [messageId, location, rotation, gryoAccel];
+}
+
+async function main(){
+    let filepath: string = "../assets/updated_beacon_output.txt";
+    let rawText: string = await readTextFile(filepath);
+    let messages: string[] = rawText.split("Message");
+
+    for (let i = 0; i < messages.length; i++) {
+        console.log(parseMessage(messages[i]));
+    }
+}
+
+main();
+
