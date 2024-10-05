c815e1dfe84ac22b8ea7d11a18975eda4c5928d0
brian <brian.w.tjahjadi@gmail.com>
2024-10-05 12:49:21 +1000

    pasrse raw text into fake message stream

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -11,7 +11,7 @@ async function readTextFile(filePath: string): Promise<string> {
 }
 
 function parseMessage(message: string): [string | null, number[] | null, number[] | null, number[] | null]  {
-    message = message.trim();
+    message = message.split("Message")[1].trim();
     let messageId: string | null = message.split("V")[0] || null;
     let location: number[] | null = message.split("L")[1]?.split("I")[0]?.slice(1,-1).split(",").map(Number) || null;
     let rotation: number[] | null = message.split("R")[1]?.split("A")[0]?.slice(1,-1).split(",").map(Number) || null;
@@ -19,13 +19,18 @@ function parseMessage(message: string): [string | null, number[] | null, number[
     return [messageId, location, rotation, gryoAccel];
 }
 
+async function parseRawMessages (filepath: string) : Promise<string[]> {
+    let rawText: string = await readTextFile(filepath);
+    let messages: string[] = rawText.split("Message").slice(1).map((message) => ("Message" + message));
+    return messages;
+}
+
 async function main(){
     let filepath: string = "../assets/updated_beacon_output.txt";
-    let rawText: string = await readTextFile(filepath);
-    let messages: string[] = rawText.split("Message");
+    let fakeMessageStream: string[] = await parseRawMessages(filepath);
 
-    for (let i = 0; i < messages.length; i++) {
-        console.log(parseMessage(messages[i]));
+    for (let i = 0; i < fakeMessageStream.length; i++) {
+        console.log(parseMessage(fakeMessageStream[i]));
     }
 }
 
