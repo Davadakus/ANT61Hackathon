456ebcbdb8e00ade87865e2742bb97158092444c
epur4192 <epur4192@uni.sydney.edu.au>
2024-10-05 23:03:51 +1000

    FPS Counter Gone

--- src/lib/SceneInit.js
+++ src/lib/SceneInit.js
@@ -50,8 +50,8 @@ export default class SceneInit {
 
     this.clock = new THREE.Clock();
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.stats = Stats();
-    document.body.appendChild(this.stats.dom);
+    // this.stats = Stats();
+    // document.body.appendChild(this.stats.dom);
 
     // ambient light which is for the whole scene
     this.ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
@@ -84,7 +84,8 @@ export default class SceneInit {
     // requestAnimationFrame(this.animate.bind(this));
     window.requestAnimationFrame(this.animate.bind(this));
     this.render();
-    this.stats.update();
+    // this.stats.update();
+    // this.stats.end();
     this.controls.update();
   }
 
