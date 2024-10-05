887a66f8a09998b3d3bc4a33bc4c99e439e6897e
epur4192 <epur4192@uni.sydney.edu.au>
2024-10-05 12:04:22 +1000

    initial wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="min-h-screen flex flex-col">
+      <h1 className="text-xl text-center font-bold"> 
+        ANT61 Beacon Data Simulation
+      </h1>
+      <div className="grid grid-cols-2 gap-4 p-4 flex-1">
+        {/* First column */}
+        <div className="flex flex-col space-y-4">
+          <div className="bg-blue-500 flex-1">Simulation</div>
+        </div>
+
+        {/* Second column */}
+        <div className="grid grid-rows-3 gap-4">
+          <div className="bg-green-500 h-full">Longitude x Latitude</div>
+          <div className="bg-green-500 h-full">Altitude Data</div>
+          <div className="bg-green-500 h-full">Parsed Data</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
